0ce06637d44a3900b46672d89edeed5ab63e0cb1
divyesh123-jain <divyeshjain318@gmail.com>
2024-03-15 14:49:01 +0530

    fixed error

--- src/background.js
+++ src/background.js
@@ -1,12 +1,12 @@
-// Listen for the extension to be installed
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed');
    });
    
-   // Listen for messages from content scripts or other parts of the extension
+
    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "toggle") {
-       // Handle the toggle action
+ 
        console.log('Toggle action received');
     }
    });
