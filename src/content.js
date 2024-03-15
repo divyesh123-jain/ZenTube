4e4709f2e27d57704e99125721959b80a4949843
divyesh123-jain <divyeshjain318@gmail.com>
2024-03-15 22:39:57 +0530

    added comment hide option

--- src/content.js
+++ src/content.js
@@ -1,5 +1,5 @@
 function hideElements() {
-
+  
     const recommendedVideos = document.querySelectorAll('ytd-watch-flexy ytd-watch-next-secondary-results-renderer');
     recommendedVideos.forEach(video => video.style.display = 'none');
 
@@ -7,15 +7,17 @@ function hideElements() {
     relatedVideos.forEach(video => video.style.display = 'none');
 
 
+    const commentsSection = document.querySelector('ytd-comments');
+    if (commentsSection) {
+        commentsSection.style.display = 'none';
+    }
+
     const homepageRecommendations = document.querySelectorAll('ytd-rich-grid-renderer');
     homepageRecommendations.forEach(recommendation => recommendation.style.display = 'none');
 
-    // Add any other elements you want to hide
 }
 
-
 window.addEventListener('load', hideElements);
-
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "toggle") {
